98d93a83cdf9004bc56f0d13f16aa1e7b1483686
bidurkandel <51921725+bidurkandel@users.noreply.github.com>
2021-02-22 14:29:53 -0800

    I am done with my project

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { Link, NavLink, useHistory } from 'react-router-dom';
 
 export default function SavedList(props) {
+  const history = useHistory();
+  const routeToHome = () => {
+    history.push("/");
+  }
   return (
     <div className="saved-list">
       <h3>Saved Movies:</h3>
       {props.list.map(movie => (
-        <span className="saved-movie">{movie.title}</span>
+        <NavLink to={`/movie/${movie.id}`} key={movie.id}>
+          <span className="saved-movie" >{movie.title}</span>
+        </NavLink>
       ))}
-      <div className="home-button">Home</div>
+      <div className="home-button" onClick={routeToHome}>Home</div>
     </div>
   );
 }
